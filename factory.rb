bae2b79878fed1b59665b8ea59497ec5462226f2
Andrew Novikov <andruha2921@gmail.com>
2016-04-12 14:35:44 +0300

    refactor

--- factory.rb
+++ factory.rb
@@ -1,23 +1,22 @@
 class Factory
-  class << self
 
-    def new(*args)
+    def self.new(*args)
 
       class_name = args.shift if args[0].is_a? String
 
-      get_symbols(args)
+      begin
+        args.map!(&:to_sym)
+      rescue NoMethodError
+        puts "Arguments must be a String or a Symbol"
+        exit
+      end
 
       klass = Class.new do
 
-        args.each do |arg|
-          attr_accessor arg
-        end
+        attr_accessor *args
 
         define_method :initialize do |*values|
-          args.each_with_index do |arg,i|
-            arg = "@"<<arg.to_s
-            instance_variable_set(arg,values[i])
-          end
+          args.each_with_index { |arg,i| instance_variable_set("@#{arg}",values[i]) }
         end
 
         yield if block_given?
@@ -34,33 +33,26 @@ class Factory
       klass
     end
 
-    def get_symbols(args)
-      args.map! do |arg|
-        arg.to_sym if arg.respond_to? :to_sym
-        raise ArgumentError('Argument must be a symbol or a String') unless arg.is_a? Symbol
-        arg
-      end
-    end
-
-  end
 end
-
-Cathe = Factory.new(:a,:b) do
+#
+Cathe = Factory.new(:a,:b, "c") do
   public
   def lol
     puts "lol"
   end
 end
 
-a = Cathe.new(1,2)
-b = Cathe.new(1,2)
+a = Cathe.new(1,2,"c")
+b = Factory::Cathe.new(1,2)
 c = Cathe.new(1)
 
-#
-p a
-
-p Cathe
-
-p a == b
-p a == c
-
+print "a[0]"; p a[0]
+print "a[1]"; p a[1]
+print "a[2]"; p a[2]
+print "a[-1]"; p a[-1]
+print "a[-2]"; p a[-2]
+print "a[-3]"; p a[-3]
+print "a['a']"; p a['a']
+print "a['@a']"; p a['@a']
+print "a[:a]"; p a[:a]
+print "a[:@a]"; p a[:@a]
